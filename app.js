afd9d9a750a12c5356f62e81347df58af26a0de5
Muhammad Muzammil <mm8699832@gmail.com>
2024-01-05 15:28:45 +0500

    initialized new project with searching object

---
+++ app.js
@@ -0,0 +1,359 @@
+var cars = {
+    honda : {
+       
+        civic : {
+            image : "https://shreepng.com/img/Inside/Car/Honda/2022%20honda%20civic%20sedan.png",
+             name : "Honda civic",
+             model : 2020,
+             price : "$9874398",
+             color : ["red","black","white"],
+             carCondition : "Automatic",
+             wheels : 4,
+        },
+        city : {
+            image : "https://www.pngmart.com/files/22/Honda-City-Download-PNG-Image.png",
+            name : "Honda city",
+             model : 2023,
+             price : "$7893489",
+             color : ["blue","black","white"],
+             carCondition : "Automatic",
+             wheels : 4,
+        },
+        HRV : {
+            image : "https://s3.amazonaws.com/di-honda-enrollment/2018-hr-v/model-image-2018-hr-v-front.png",
+            name : "Honda HRV",
+             model : 2016,
+             price : "$87934987",
+             color : ["red","black","white"],
+             carCondition : "Automatic",
+             wheels : 4,
+        },
+        Accord : {
+            image : "https://purepng.com/public/uploads/large/purepng.com-blue-honda-accord-hybrid-carcarvehicletransporthonda-961524653570veqwm.png",
+            name : "Honda Accord",
+             model : 2019,
+             price : "$879387",
+             color : ["red","black","white"],
+             carCondition : "Automatic",
+             wheels : 4,
+        },
+        RidgeLine : {
+            image : "https://di-uploads-pod3.s3.amazonaws.com/silkohonda/uploads/2016/10/2017-Honda-Ridgeline-RT.png",
+            name : "Honda ridgeline",
+             model : 2010,
+             price : "$83283887",
+             color : ["blue","black","white"],
+             carCondition : "Automatic",
+             wheels : 4,
+        },
+        Amazee : {
+            image : "https://www.nttgroup.co.za/wp-content/uploads/2019/08/Honda-Amaze-PNG-Download-Image.png",
+            name : "Honda Amazee",
+             model : 2023,
+             price : "$879387",
+             color : ["red","blue","white"],
+             carCondition : "Automatic",
+             wheels : 4,
+        },
+     Honda_WRV : {
+            image : "https://static.wixstatic.com/media/5f0d5f_888f59ecfa2c4b91b0b0ba8e2a66edc7~mv2.png/v1/fill/w_600,h_338,al_c,q_85,enc_auto/5f0d5f_888f59ecfa2c4b91b0b0ba8e2a66edc7~mv2.png",
+            name : "Honda Accord",
+             model : 2019,
+             price : "$879387",
+             color : ["red","black","white"],
+             carCondition : "Automatic",
+             wheels : 4,
+        },
+        Brio : {
+            image : "https://asset.honda-indonesia.com/variants/images/N64b8HcaD1RuGJGouNsrWrWBSE5yQ3I2wrQxpipj.png",
+            name : "Honda Brio",
+             model : 2023,
+             price : "$3793487",
+             color : ["red","black","white"],
+             carCondition : "Automatic",
+             wheels : 4,
+        },
+        NSX : {
+            image : "https://purepng.com/public/uploads/large/purepng.com-acura-nsx-carcarvehicletransportacura-961524640438ry3jv.png",
+            name : "Honda NSX",
+             model : 2008,
+             price : "$879879387",
+             color : ["red","blue","white"],
+             carCondition : "Automatic",
+             wheels : 4,
+        },
+        Elavate : {
+            image : "https://ringroadhonda.com/wp-content/uploads/2023/06/1686563013-c4b6b76c3874e41609bdb4e15c8653c5-ELEVATE-1.png",
+            name : "Honda NSX",
+            model : 2008,
+            price : "$879879387",
+            color : ["red","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+        },
+
+    },
+
+    toyota : {
+           corolla : {
+            image : "https://freepngimg.com/thumb/toyota/8-2-toyota-car-png-picture.png",
+            name : " toyota corolla",
+            model : 2023,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+           },
+           camry : {
+            image : "https://i.pinimg.com/originals/56/f7/55/56f755ce852fc23de4ca0dd6e74361ea.png",
+            name : "toyota camry",
+            model : 2020,
+            price : "$2379387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+           },
+           prius : {
+            image : "https://freebiescloud.com/wp-content/uploads/2021/02/TOYOTA-PRIUS-2022-2.png", 
+            name : " toyota prius",
+            model : 2023,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+           },
+           avalon : {
+            image : "https://platform.cstatic-images.com/xlarge/in/v2/stock_photos/e321f91f-c3df-48b2-9bb7-64c074164f6f/1976a626-38e3-44ca-9515-c61501b753c5.png",
+            name : "toyota avalon",
+            model : 2023,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+           },
+           for_Runner : {
+            image : "https://www.motortrend.com/uploads/sites/10/2015/11/2011-toyota-4runner-sr5-4x4-v6-suv-angular-front.png?fit=around%7C875:492.1875",
+            name : "toyota 4runner",
+            model : 2010,
+            price : "$8779873247",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+           } ,
+           corolla_cross :{
+            image : "https://buycarsudan.s3.amazonaws.com/uploads/post/featured_image/25/corolla-cross-2022-xle.png",
+            name : "corolla-cross",
+            model : 2022,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+           },
+           crown : {
+            image : "https://crdms.images.consumerreports.org/c_lfill,w_720,q_auto,f_auto/prod/cars/cr/model-years/15413-2023-toyota-crown",
+            name : "toyota crown",
+            model : 2023,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4, 
+           },
+           landCruiser : {
+            image : "https://purepng.com/public/uploads/large/purepng.com-toyota-land-cruiser-white-carcarvehicletransporttoyota-961524668359oxuc6.png",
+            name : "landcruiser",
+            model : 2015,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+           },
+           Sequoia : {
+            image : "https://images.carprices.com/pricebooks_data/usa/colorized/2023/Toyota/View2/Sequoia/TRD_Pro/7953_4W5.png",
+            name : " toyota Sequoia",
+            model : 2015,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+           },
+           fortuner : {
+            image : "https://5.imimg.com/data5/UH/FL/SO/GLADMIN-9844717/fortuner-500x500.png",
+            name : " toyota fortuner",
+            model : 2015,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+           },
+    },
+    suzuki : {
+           swift : {
+            image : "https://pngimg.com/d/suzuki_PNG12264.png",
+            name : " suzuki swift",
+            model : 2015,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+           }, 
+           alto : {
+            image : "https://5.imimg.com/data5/YR/TQ/GLADMIN-33713197/maruti-suzuki-alto-800-car.png",
+            name : " suzuki alto",
+            model : 2019,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+           },  
+         S_Presso :{
+            image: "https://imgd.aeplcdn.com/1200x900/n/cw/ec/48312/exterior-right-front-three-quarter.jpeg",
+            name : " suzuki s-presso",
+            model : 2014,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+         } ,
+         celerio : {
+            image : "https://autolp.gr/wp-content/uploads/2018/08/SuzukiCelerio.png",
+            name : " suzuki celerio",
+            model : 2008,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+         },
+         maruti_XL6 : {
+            image : "https://i0.wp.com/gomechanic.in/blog/wp-content/uploads/2019/08/maruti-suzuki-xl6-magma-gray-e1606200834568.png?resize=700%2C367&ssl=1",
+            name : " suzuki maruti XL6",
+            model : 2013,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+         },
+         Grand_vatira : {
+            image : "https://www.suzukiauto.co.za/hs-fs/hubfs/GV.png?width=2000&height=1406&name=GV.png",
+            name : " suzuki  grand vatira",
+            model : 2023,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+         },
+         fronx :{
+            image : "https://zawarcars.com/wp-content/uploads/2023/05/image-750x420.png",
+            name : " suzuki fronx",
+            model : 2022,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+         },
+         Baleno : {
+            image : "https://images.carandbike.com/car-images/colors/maruti-suzuki/baleno/maruti-suzuki-baleno-celestial-blue.png?v=1645703898",
+            name : " suzuki baleno",
+            model : 2013,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+         },
+         mehran : {
+            image : "https://cache2.pakwheels.com/system/car_generation_pictures/2873/original/Suzuki_Mehran_2012.jpg?1444111663",
+            name : " suzuki mehran",
+            model : 2010,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+         },
+         cultus : {
+            image : "https://suzukifortmotors.com/wp-content/uploads/2018/03/CELERIO-WHITE-719x463.png",
+            name : " suzuki cultus",
+            model : 2013,
+            price : "$8779387",
+            color : ["black","blue","white"],
+            carCondition : "Automatic",
+            wheels : 4,
+         },
+    },
+   
+}
+var compony = document.getElementById("company")
+var carName = document.getElementById("car-name")
+var allCarsData = document.getElementById("all-cars-data")
+compony.innerHTML = `<option  value = "">Select Company </option>`
+carName.innerHTML = `<option  value = "">Select cars </option>`
+
+// console.log(carName);
+for(var key1 in cars){
+  
+   compony.innerHTML += `<option  value = "${key1}" >${ key1.toUpperCase()}</option>`
+   for(var key2 in cars[key1]){
+      console.log(cars[key1][key2].color)
+      var colorsDiv = "";
+      // for(var i = 0; i < cars[key1][key2].color.length; i++){
+      //       console.log(cars[key1][key2].color[i]);
+      //       colorsDiv += `<div class="colors" style="border: 1px solid${cars[key1][key2].color[i]}; "></div>`
+      // }
+      // console.log(colorsDiv)
+     allCarsData.innerHTML +=`<div  class="col mb-3">
+     <div class="card" style="width: 18rem; border-radius:10px;">
+     <img class="car-images" src = "${cars[key1][key2].image}"/>
+       <div class="card-body">
+         <h5 style =  "background-color : #05e389;  padding: 5px; color : #fff; border-radius: 10px; text-align:center; height : 40px" class="card-title">${cars[key1][key2].name}</h5>
+         <h3 class="card-subtitle mb-2 text-muted">${cars[key1][key2].model}</h3>
+         
+       
+       <h3 style = "background-color: #23036a; padding: 5px; color : #fff; border-radius: 10px; text-align:center; height : 40px;">${cars[key1][key2].price}</h3>
+        <div>${colorsDiv}</div>
+       </div>
+     </div>
+   </div>`
+   }
+}
+
+function companyChange(){
+   carName.innerHTML = "";
+   carName.innerHTML = `<option  value = "">Select cars </option>`
+   for(var key in cars[compony.value]){
+  
+    carName.innerHTML += `<option value = "${key}" > ${key.toUpperCase()}</option>`
+   }
+}
+function filterCars(){
+   var logo_image = document.querySelector(".logo-image")
+   var bigImage = document.querySelector(".big-image");
+   allCarsData.style.display = "none";
+   logo_image.style.display = "none";
+console.log(cars[compony.value][carName.value].image);
+bigImage.innerHTML = `<div class = "container">
+<div class = "small-container">
+        <img class = 'filter-image' src = "${cars[compony.value][carName.value].image}"/>
+        
+        <h2>${cars[compony.value][carName.value].name}</h2>
+        <h2>${cars[compony.value][carName.value].model}</h2>
+        <h2>${cars[compony.value][carName.value].price}</h2>
+       
+     
+        </div></div>`
+        
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
